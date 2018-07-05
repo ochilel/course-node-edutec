d3b779bb642d233b23dcdcc2f0115d3ee11cd45a
Oscar Chilel <oskr.chilel@gmail.com>
2018-07-04 20:02:52 -0600

    create animals api

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var animalRoutes = require('./routes/animal');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api', animalRoutes);
+
+app.get ('/test', (req, res) => {
+    res.status(200).send({
+        message: 'mi primer'
+    });
+});
+
+module.exports = app;
